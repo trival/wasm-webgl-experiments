6b4839bf8b6cc802b74056874c5b845f30201714
Thomas Gorny <thomas.gorny@pm.me>
2023-06-22 18:53:31 +0200

    tweak shader

--- sketches/balls/src/render.ts
+++ sketches/balls/src/render.ts
@@ -1,18 +1,10 @@
 import {
 	assign,
 	defMain,
-	input,
-	Mat3Sym,
-	Mat4Sym,
 	mul,
 	normalize,
-	output,
-	program,
-	uniform,
 	vec3,
-	Vec3Sym,
 	vec4,
-	Vec4Sym,
 	float,
 } from '@thi.ng/shader-ast'
 import { diffuseLighting, halfLambert } from '@thi.ng/shader-ast-stdlib'
@@ -44,7 +36,7 @@ const shader = defShader({
 	},
 	vs: (gl, uniforms, inp, out) => [
 		defMain(() => [
-			assign(gl.gl_Position, mul(uniforms.camera, vec4(inp.position, 1.0))),
+			assign(gl.gl_Position, mul(uniforms.camera, vec4(inp.position, 1))),
 			assign(gl.gl_PointSize, float(2)),
 			assign(out.vColor, inp.color),
 			assign(out.vNormal, mul(uniforms.normalMatrix, inp.normal)),
@@ -59,9 +51,9 @@ const shader = defShader({
 						halfLambert(normalize(inp.vNormal), uniforms.light),
 						inp.vColor,
 						vec3(1, 1, 1),
-						vec3(0.1, 0.0, 0.0),
+						vec3(0.1, 0, 0),
 					),
-					1.0,
+					1,
 				),
 			),
 		]),
